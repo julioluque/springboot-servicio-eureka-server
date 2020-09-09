acfba33e4f2198a16334e716a8424299f8841091
julioluque <julio.a.luque@gmail.com>
2020-09-09 05:47:45 -0300

    Habilitando Eureka Server -> Run -> localhost:8761 devuelve dashboard

--- src/main/java/com/jluque/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/jluque/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.jluque.springboot.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringbootServicioEurekaServerApplication {
 
 	public static void main(String[] args) {
