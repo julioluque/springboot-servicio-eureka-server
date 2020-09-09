2c3734c2b316838014994e2a5aadc63678e869b8
julioluque <julio.a.luque@gmail.com>
2020-09-09 05:28:56 -0300

    First Commit

---
+++ src/test/java/com/jluque/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jluque.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
